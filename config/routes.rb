64ffc1db78710f7094987616f1345bc216994e84
naoshi.murayama <go_convination9@yahoo.co.jp>
2019-11-24 13:03:43 +0000

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
+  get 'new/show'
+  get 'new/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
-  resources :tasks
   
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  get "signup", to: "users#new"
+  resources :tasks
+  resources :users, only: [:show, :new, :create] 
 end
+
